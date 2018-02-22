14fb1710fbc16d934cb9b5fc1a452e5bf4226b5f
BrandonDG <brandon.gillespie08@gmail.com>
2018-02-21 19:29:56 -0800

    Basic multithreaded, continuous connection between client and server

--- client/client.c
+++ client/client.c
@@ -10,18 +10,22 @@
 #include <arpa/inet.h>
 #include <time.h>
 #include <sys/time.h>
+#include <omp.h>
 
 #define SERVER_TCP_PORT 8005
-#define BUFLEN			    80
+#define BUFLEN			    1024
+#define TRUE            1
 
 int main(int argc, char **argv) {
 	struct hostent	   *hp;
 	struct sockaddr_in server;
   struct timeval     begin, end;
-  int                sd, port, bytes_to_read, n;
+  int                sd, port, bytes_to_read, n, cn;
 	char               *host, *bp, **pptr;
-	char               str[16], rbuf[BUFLEN], sbuf[BUFLEN], lbuf[BUFLEN];
+	char               str[16], rbuf[BUFLEN], lbuf[BUFLEN];
   FILE               *fp;
+
+  cn = 1;
 
   switch(argc) {
     case 2:
@@ -30,61 +34,91 @@ int main(int argc, char **argv) {
     break;
     case 3:
       host = argv[1];
-      port = atoi(argv[2]);
+      port = SERVER_TCP_PORT;
+      cn = atoi(argv[2]);
     break;
     default:
-      fprintf(stderr, "[command] [ip] [port]");
+      fprintf(stderr, "[command] [ip] [port]\n");
       exit(1);
     break;
   }
 
-  if ((fp = fopen("client_results", "a")) == 0) {
-    fprintf(stderr, "client fopen\n");
-    exit(1);
+  if ((fp = fopen("client_results", "w")) == 0) {
+    fprintf(stderr, "client_driver fopen\n");
   }
 
-  if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-    perror("Failed to create socket");
-    exit(1);
+  omp_set_num_threads(cn);
+  #pragma omp parallel private(sd)
+  {
+    /*
+    if ((fp = fopen("client_results", "a")) == 0) {
+      fprintf(stderr, "client fopen\n");
+      exit(1);
+    } */
+
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+      perror("Failed to create socket");
+      exit(1);
+    }
+    bzero((char *)&server, sizeof(struct sockaddr_in));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    if ((hp = gethostbyname(host)) == NULL) {
+      fprintf(stderr, "Unknown server address\n");
+      exit(1);
+    }
+    bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+
+    if (connect(sd, (struct sockaddr *)&server, sizeof(server)) == -1) {
+      fprintf(stderr, "Can't connect to server\n");
+      perror("Connect");
+      exit(1);
+    }
+    pptr = hp->h_addr_list;
+
+    //sbuf = "Hello, my name is Brandon\0";
+    char sbuf[BUFLEN] = { 'A' };
+
+    while (TRUE) {
+      gettimeofday(&begin, NULL);
+      send(sd, sbuf, BUFLEN, 0);
+
+    	bp = rbuf;
+    	bytes_to_read = BUFLEN;
+      recv(sd, bp, bytes_to_read, MSG_WAITALL);
+      gettimeofday(&end, NULL);
+
+      n = end.tv_usec - begin.tv_usec;
+      if (n < 0) {
+        n *= - 1;
+      }
+      sprintf(lbuf, "%s %d %ld %lu.%06d", inet_ntop(hp->h_addrtype, *pptr, str, sizeof(str)),
+                sd, sizeof(sbuf), (end.tv_sec - begin.tv_sec), n);
+      printf("%s\n", lbuf);
+    }
+
+    /*
+    gettimeofday(&begin, NULL);
+    send(sd, sbuf, BUFLEN, 0);
+
+    bp = rbuf;
+    bytes_to_read = BUFLEN;
+    recv(sd, bp, bytes_to_read, MSG_WAITALL);
+    gettimeofday(&end, NULL);
+
+    n = end.tv_usec - begin.tv_usec;
+    if (n < 0) {
+      n *= - 1;
+    }
+    sprintf(lbuf, "%s %d %ld %lu.%06d", inet_ntop(hp->h_addrtype, *pptr, str, sizeof(str)),
+              sd, sizeof(sbuf), (end.tv_sec - begin.tv_sec), n);
+    printf("%s\n", lbuf); */
+    fprintf(fp, "%s\n", lbuf);
+
+    fflush(stdout);
+    close(sd);
+    //fclose(fp);
   }
-  bzero((char *)&server, sizeof(struct sockaddr_in));
-  server.sin_family = AF_INET;
-  server.sin_port = htons(port);
-  if ((hp = gethostbyname(host)) == NULL) {
-    fprintf(stderr, "Unknown server address\n");
-    exit(1);
-  }
-  bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
-
-  if (connect(sd, (struct sockaddr *)&server, sizeof(server)) == -1) {
-    fprintf(stderr, "Can't connect to server");
-    perror("Connect");
-    exit(1);
-  }
-  pptr = hp->h_addr_list;
-
-  sbuf[0] = 'H';
-  sbuf[1] = '\0';
-
-  gettimeofday(&begin, NULL);
-  send(sd, sbuf, BUFLEN, 0);
-
-	bp = rbuf;
-	bytes_to_read = BUFLEN;
-  recv(sd, bp, bytes_to_read, MSG_WAITALL);
-  gettimeofday(&end, NULL);
-
-  n = end.tv_usec - begin.tv_usec;
-  if (n < 0) {
-    n *= - 1;
-  }
-  sprintf(lbuf, "%s %d %ld %lu.%06d", inet_ntop(hp->h_addrtype, *pptr, str, sizeof(str)),
-            sd, sizeof(sbuf), (end.tv_sec - begin.tv_sec), n);
-  printf("%s\n", lbuf);
-  fprintf(fp, "%s\n", lbuf);
-
-  fflush(stdout);
-  close(sd);
   fclose(fp);
 
   return(0);
--- server/server.c
+++ server/server.c
@@ -1,4 +1,4 @@
-#include <pthread.h>
+//#include <pthread.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -7,12 +7,13 @@
 #include <strings.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <omp.h>
 
 #define SERVER_TCP_PORT 8005
-#define BUFLEN 80
-#define TRUE 1
+#define BUFLEN          1024
+#define TRUE            1
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+//pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void *client_function(void*);
 
@@ -23,10 +24,13 @@ typedef struct thread_param {
 
 int main(int argc, char **argv) {
   int                sd, new_sd, client_len, port;
-  //int                bytes_to_read;
-  //char               *bp, buf[BUFLEN], lbuf[BUFLEN];
+  int                bytes_to_read, thread_num;
+  char               *bp, buf[BUFLEN], lbuf[BUFLEN];
   struct sockaddr_in server, client;
   FILE               *fp;
+  int e;
+
+  thread_num = 0;
 
   switch (argc) {
     case 1:
@@ -61,24 +65,59 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  listen(sd, 5);
+  listen(sd, 4096);
 
-  while (TRUE) {
-    client_len = sizeof(client);
-    if ((new_sd = accept(sd, (struct sockaddr *)&client, &client_len)) == -1) {
-      fprintf(stderr, "Can't accept client\n");
-      exit(1);
+  #pragma omp parallel private(thread_num, new_sd, fp)
+  {
+    while (TRUE) {
+      client_len = sizeof(client);
+      if ((new_sd = accept(sd, (struct sockaddr *)&client, &client_len)) == -1) {
+        fprintf(stderr, "Can't accept client\n");
+        exit(1);
+      }
+      if ((fp = fopen("server_results", "a")) == 0) {
+        fprintf(stderr, "server fopen\n");
+        exit(1);
+      }
+      thread_num = omp_get_thread_num() + 1;
+      // Tomorrow try while (recv) so maybe when the conncetion is broken
+      // it will stop. We'll see if that works.
+      // Or give a maximum amount of data sent before it cuts out, would have to
+      // be consistent on both client and server
+      /*
+      while (TRUE) {
+        bp = buf;
+        bytes_to_read = BUFLEN;
+        recv(new_sd, bp, bytes_to_read, MSG_WAITALL);
+        send(new_sd, buf, BUFLEN, 0);
+        sprintf(lbuf, "%s %u %ld", inet_ntoa(client.sin_addr), thread_num, sizeof(buf));
+        printf("%s\n", lbuf);
+      } */
+      /*
+      bp = buf;
+      bytes_to_read = BUFLEN;
+      recv(new_sd, bp, bytes_to_read, MSG_WAITALL);
+      send(new_sd, buf, BUFLEN, 0);
+      sprintf(lbuf, "%s %u %ld", inet_ntoa(client.sin_addr), thread_num, sizeof(buf));
+      printf("%s\n", lbuf);
+      fprintf(fp, "%s\n", lbuf); */
+      bp = buf;
+      bytes_to_read = BUFLEN;
+      while ((e = recv(new_sd, bp, bytes_to_read, MSG_WAITALL)) > 0) {
+        //recv(new_sd, bp, bytes_to_read, MSG_WAITALL);
+        send(new_sd, buf, BUFLEN, 0);
+        sprintf(lbuf, "%s %u %ld", inet_ntoa(client.sin_addr), thread_num, sizeof(buf));
+        printf("%s\n", lbuf);
+      }
+      fprintf(fp, "%s\n", lbuf);
+
+      close(new_sd);
+      fclose(fp);
     }
-    pthread_t client_thread;
-    struct thread_param thp;
-    thp.socket = new_sd;
-    thp.ip = inet_ntoa(client.sin_addr);
-    pthread_create(&client_thread, NULL, client_function, &thp);
-    pthread_join(client_thread, NULL);
   }
   return 0;
 }
-
+/*
 void *client_function(void *thp) {
   char *bp, buf[BUFLEN], lbuf[BUFLEN];
   int  bytes_to_read;
@@ -101,4 +140,4 @@ void *client_function(void *thp) {
 
   close(local_thp->socket);
   fclose(fp);
-}
+} */
